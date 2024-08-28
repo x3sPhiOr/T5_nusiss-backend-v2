9586ad16607a6bd26e9d168a570d8099b4769625
Shen Kai Wong <shenkai.wong@ncs.com.sg>
2024-08-28 15:34:43 +0800

    create DTOs

---
+++ ReservationApp.Data/BuffetTableDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace BookStoreApi.ReservationApp.Data
+{
+    public class BuffetTableDTO
+    {
+        public int TableID { get; set; }
+        public int TableNumber { get; set; }
+        public int NumberOfSeats { get; set; }
+        public string Location { get; set; }
+    }
+}
---
+++ ReservationApp.Data/ReservationWithCustomerDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace BookStoreApi.ReservationApp.Data
+{
+    public class ReservationWithCustomerDTO
+    {
+        public int ReservationID { get; set; }
+        public DateTime ReservationDate { get; set; }
+        public CustomerDTO Customer { get; set; }
+    }
+}

4a93b8f6ef11f5dce3028699ceadd099bb9ce46f
Shen Kai Wong <shenkai.wong@ncs.com.sg>
2024-08-28 16:23:38 +0800

    prepared CustomerController V2

--- ReservationApp.Controllers/CustomersController.cs
+++ ReservationApp.Controllers/CustomersController.cs
@@ -15,11 +15,114 @@ namespace BookStoreApi.ReservationApp.Controllers
     public class CustomersController : ControllerBase
     {
         private readonly ReservationContext _context;
-
         public CustomersController(ReservationContext context)
         {
             _context = context;
         }
+
+        #region v2
+        [HttpPost("v2")]
+        public async Task<ActionResult<Customer>> PostCustomer_v2(CustomerDTO dto)
+        {
+            //_context.Customers.Add(customer);
+            //await _context.SaveChangesAsync();
+
+            //return CreatedAtAction("GetCustomer", new { id = customer.CustomerID }, customer);
+
+            var customer = new Customer
+            {
+                CustomerID = dto.CustomerID,
+                FirstName = dto.FirstName,
+                LastName = dto.LastName,
+                Email = dto.Email,
+                PhoneNumber = dto.PhoneNumber
+            };
+
+            _context.Customers.Add(customer);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetCustomer", new { id = customer.CustomerID }, customer);
+        }
+
+        [HttpGet("v2")]
+        public async Task<ActionResult<IEnumerable<CustomerDTO>>> GetCustomers_v2()
+        {
+            var customers = await _context.Customers
+                .Select(c => new CustomerDTO
+                {
+                    CustomerID = c.CustomerID,
+                    FirstName = c.FirstName,
+                    LastName = c.LastName,
+                    Email = c.Email,
+                    PhoneNumber = c.PhoneNumber
+                    // Map other properties as needed, but exclude navigation properties
+                })
+                .ToListAsync();
+
+            return Ok(customers);
+        }
+
+        // no change from v1
+        [HttpGet("v2/{id}")]
+        public async Task<ActionResult<Customer>> GetCustomer_v2(int id)
+        {
+            var customer = await _context.Customers.FindAsync(id);
+
+            if (customer == null)
+            {
+                return NotFound();
+            }
+
+            return customer;
+        }
+
+        [HttpPut("v2/{id}")]
+        public async Task<IActionResult> PutCustomer_v2(int id, CustomerDTO customerDto)
+        {
+            if (id != customerDto.CustomerID)
+            {
+                return BadRequest();
+            }
+
+            // Retrieve the existing customer from the database
+            var customer = await _context.Customers.FindAsync(id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+
+            // Map the properties from the DTO to the entity
+            customer.FirstName = customerDto.FirstName;
+            customer.LastName = customerDto.LastName;
+            customer.Email = customerDto.Email;
+            customer.PhoneNumber = customerDto.PhoneNumber;
+            // Map other properties as needed, excluding navigation properties
+
+            // Mark the entity as modified
+            _context.Entry(customer).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CustomerExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+        #endregion
+
+
+
 
         // GET: api/Customers
         [HttpGet]
@@ -84,28 +187,6 @@ namespace BookStoreApi.ReservationApp.Controllers
             return CreatedAtAction("GetCustomer", new { id = customer.CustomerID }, customer);
         }
 
-        [HttpPost("v2")]
-        public async Task<ActionResult<Customer>> PostCustomer_v2(CustomerDTO dto)
-        {
-            //_context.Customers.Add(customer);
-            //await _context.SaveChangesAsync();
-
-            //return CreatedAtAction("GetCustomer", new { id = customer.CustomerID }, customer);
-
-            var customer = new Customer
-            {
-                CustomerID = dto.CustomerID,
-                FirstName = dto.FirstName,
-                LastName = dto.LastName,
-                Email = dto.Email,
-                PhoneNumber = dto.PhoneNumber
-            };
-
-            _context.Customers.Add(customer);
-            await _context.SaveChangesAsync();
-
-            return CreatedAtAction("GetCustomer", new { id = customer.CustomerID }, customer);
-        }
 
         // DELETE: api/Customers/5
         [HttpDelete("{id}")]
